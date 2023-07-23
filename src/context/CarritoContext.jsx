a5e1e218c3959ebd7ee5c3b60588257d9de29bf6
Joquin Lamiño <joaquinlaminio.1@gmail.com>
2023-07-22 21:39:38 -0300

    prueba

--- src/context/CarritoContext.jsx
+++ src/context/CarritoContext.jsx
@@ -1,8 +1,6 @@
-//1) Voy a importar el hook useState y createContext que me permite crear un contexto que va a almacenar toda la lógica de mi carrillo de compras. 
-
 import { useState, createContext } from "react";
 
-//2) Creamos el nuevo contexto. 
+
 
 export const CarritoContext = createContext({
     carrito: [],
@@ -10,28 +8,22 @@ export const CarritoContext = createContext({
     cantidadTodal: 0
 })
 
-//3) Creamos un componente llamado "CarritoProvider". 
-//También lo pueden encontrar como "proveedor de contextos". 
 
 export const CarritoProvider = ({children}) => {
-    //4) Creamos un estado local "carrito, total y cantidad total" con el hook useState.
     const [carrito, setCarrito] = useState([]);
     const [total, setTotal] = useState(0);
     const [cantidadTotal, setCantidadTotal] = useState(0);
 
-    //No se olviden de esto: verifiquen el carrito por consola. 
+
     console.log(carrito);
 
-    //5) Agregamos algunos métodos al proveedor de contexto para manipular el carrito de compras: 
 
-    //Función agregar al carrito: 
 
     const agregarProducto = (item, cantidad) => {
         const productoExistente = carrito.find(prod => prod.item.id === item.id);
 
         if(!productoExistente) {
             setCarrito(prev => [...prev, {item, cantidad}]);
-            //La sintaxis: prev => [...prev, {item, cantidad}] la uso para crear un nuevo array a partir del estado anterior del carrito (prev) y agregar un nuevo objeto que representa el nuevo producto. 
             setCantidadTotal(prev => prev + cantidad);
             setTotal(prev => prev + (item.precio * cantidad));
         } else {
